547c7579e3d4faefe5742444d70b00f24fe521c6
jubair <63695065+jubair754@users.noreply.github.com>
2025-08-18 09:47:54 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,12 @@
 plugins {
     id("com.android.application") version "8.6.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
 }
-tasks.register<Wrapper>("wrapper") {
-    gradleVersion = "8.7"
-    distributionType = Wrapper.DistributionType.ALL
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
 }
